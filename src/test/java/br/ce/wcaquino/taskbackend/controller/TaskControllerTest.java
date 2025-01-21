bbd318c4bea0dd20a9f0d2d61273e5011b1fa68c
nathaliamaimone <nathalia.pmaimone@gmail.com>
2025-01-21 08:46:11 -0300

    Add another version of mockito

--- src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -3,50 +3,78 @@ package br.ce.wcaquino.taskbackend.controller;
 import java.time.LocalDate;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import br.ce.wcaquino.taskbackend.model.Task;
+import br.ce.wcaquino.taskbackend.repo.TaskRepo;
 import br.ce.wcaquino.taskbackend.utils.ValidationException;
 
 public class TaskControllerTest {
-		
+	
+	@Mock
+	private TaskRepo taskRepo;
+	
+	@InjectMocks
+	private TaskController controller;
+	
+	@SuppressWarnings("deprecation")
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+
 	@Test
-	public void shouldFailToSaveTaskWithoutDescription() {
+	public void shouldNotSaveTaskWithoutDescription() {
 		Task todo = new Task();
-//		todo.setTask("Description");
 		todo.setDueDate(LocalDate.now());
-		TaskController controller = new TaskController();
 		try {
 			controller.save(todo);
+			Assert.fail("It shouldn't get to this point.");
 		} catch (ValidationException e) {
 			Assert.assertEquals("Fill the task description", e.getMessage());
 		}
-	} 
-
+		
+	}
+	
 	@Test
-	public void shouldFailToSaveTaskWithoutDueDate() {
+	public void shouldNotSaveTaskWithoutDate() {
 		Task todo = new Task();
-		todo.setTask("Description");
+		todo.setTask("Descrição");
 //		todo.setDueDate(LocalDate.now());
-		TaskController controller = new TaskController();
 		try {
 			controller.save(todo);
+			Assert.fail("It shouldn't get to this point.");
 		} catch (ValidationException e) {
 			Assert.assertEquals("Fill the due date", e.getMessage());
 		}
 	}
-
+	
 	@Test
-	public void shouldFailToSaveTaskWithPastDueDate() {
+	public void shouldNotSaveTaskWithPastDate() {
 		Task todo = new Task();
-		todo.setTask("Description");
-		todo.setDueDate(LocalDate.of(2010,01,01));
-		TaskController controller = new TaskController();
+		todo.setTask("Descrição");
+		todo.setDueDate(LocalDate.of(2010, 01, 01));
 		try {
 			controller.save(todo);
+			Assert.fail("It shouldn't get to this point.");
 		} catch (ValidationException e) {
 			Assert.assertEquals("Due date must not be in past", e.getMessage());
 		}
 	}
 	
+	@Test
+	public void shouldSaveTaskWithSuccessfully() throws ValidationException {
+		Task todo = new Task();
+		todo.setTask("Descrição");
+		todo.setDueDate(LocalDate.now());
+		controller.save(todo);
+		
+		Mockito.verify(taskRepo).save(todo);
+	}
 }
